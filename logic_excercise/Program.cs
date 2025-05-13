eaaa48a6108ce8dc61afb2a6748099b9956baa5f
fdosisme <fernnado94.mail@gmail.com>
2025-05-13 11:25:26 +0700

    feat: logic excercise

---
+++ logic_excercise/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace login_exercise
+{
+ class Program
+   {
+       static void Main(string[] args)
+       {
+        int n = 15;
+          for (int i = 1; i <= n; i++)
+          {
+            if (i%5 == 0 && i%3 == 0)
+                {
+          Console.Write("foobar");
+                    }else if (i%3 == 0)
+                {
+          Console.Write("foo");
+                    }else if (i%5 == 0)
+                {
+          Console.Write("bar");
+                    }else{
+                        Console.Write(i);
+                    }
+                    if (i != n )
+                    {
+          Console.Write(", ");
+                        
+                    }
+          }
+       }
+   }
+}
